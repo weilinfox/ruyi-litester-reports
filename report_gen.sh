30aaefc6e9cb25fa7897f763edc6f0f5ae3ad76b
weilinfox <caiweilin@iscas.ac.cn>
2024-12-17 15:50:06 +0800

    Do not pass report_dir
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- report_gen.sh
+++ report_gen.sh
@@ -21,7 +21,6 @@ report_config_my_sh="$SELF_PATH"/report_my_configs.sh
 report_tmpl_dir="$SELF_PATH"/report_tmpl
 
 temp_dir=/tmp/ruyi_report
-report_dir=$(realpath ${2:?})
 report_name=`jq -r .\"${1:?}\" $report_name_js`
 log_name=`jq -r .\"${1:?}\" $log_name_js`
 
@@ -39,24 +38,22 @@ log_name=`jq -r .\"${1:?}\" $log_name_js`
 }
 
 [[ -d $temp_dir ]] && rm -rf $temp_dir
-mkdir $report_dir
-
-[[ -d $report_dir ]] || mkdir -p $report_dir
+mkdir $temp_dir
 
 cp ${report_tmpl_dir}/*.md ${report_tmpl_dir}/${1:?}/*.md $temp_dir/
 
 for f in `ls ${temp_dir} | sort`; do
 	echo Find template ${temp_dir}/$f
-	cat ${temp_dir}/$f >> $report_dir/my.md
+	cat ${temp_dir}/$f >> $TEST_LITESTER_PATH/my.md
 done
 rm -rf $temp_dir
 
-sed -i "s/{{ruyi_arch}}/${TEST_ARCH:?}/g" $report_dir/my.md
-sed -i "s/{{ruyi_version}}/${TEST_VERSION:?}/g" $report_dir/my.md
-sed -i "s|{{ruyi_link}}|${TEST_PKG_LINK:?}|g" $report_dir/my.md
-sed -i "s|{{ruyitest_repo}}|${TEST_REPO:?}|g" $report_dir/my.md
-sed -i "s|{{ruyitest_repo_raw}}|${TEST_REPO_RAW:?}|g" $report_dir/my.md
-sed -i "s/{{log_name}}/$log_name/g" $report_dir/my.md
+sed -i "s/{{ruyi_arch}}/${TEST_ARCH:?}/g" $TEST_LITESTER_PATH/my.md
+sed -i "s/{{ruyi_version}}/${TEST_VERSION:?}/g" $TEST_LITESTER_PATH/my.md
+sed -i "s|{{ruyi_link}}|${TEST_PKG_LINK:?}|g" $TEST_LITESTER_PATH/my.md
+sed -i "s|{{ruyitest_repo}}|${TEST_REPO:?}|g" $TEST_LITESTER_PATH/my.md
+sed -i "s|{{ruyitest_repo_raw}}|${TEST_REPO_RAW:?}|g" $TEST_LITESTER_PATH/my.md
+sed -i "s/{{log_name}}/$log_name/g" $TEST_LITESTER_PATH/my.md
 
 mkdir "${TEST_LITESTER_PATH}"/logs
 # format test logs name
@@ -91,12 +88,12 @@ rmdir --ignore-fail-on-non-empty "${TEST_LITESTER_PATH}"/logs_failed
 if [ "$ruyi_failed" -gt 0 ]; then
 	ruyi_conclusion="此处添加评论"
 fi
-sed -i "s/{{ruyi_conclusion}}/$ruyi_conclusion/g" $report_dir/my.md
-sed -i "s/{{ruyi_testsuites}}/$ruyi_testsuites/g" $report_dir/my.md
-sed -i "s/{{ruyi_success}}/$ruyi_success/g" $report_dir/my.md
-sed -i "s/{{ruyi_failed}}/$ruyi_failed/g" $report_dir/my.md
+sed -i "s/{{ruyi_conclusion}}/$ruyi_conclusion/g" $TEST_LITESTER_PATH/my.md
+sed -i "s/{{ruyi_testsuites}}/$ruyi_testsuites/g" $TEST_LITESTER_PATH/my.md
+sed -i "s/{{ruyi_success}}/$ruyi_success/g" $TEST_LITESTER_PATH/my.md
+sed -i "s/{{ruyi_failed}}/$ruyi_failed/g" $TEST_LITESTER_PATH/my.md
 # rename md report
-mv -v $report_dir/my.md $report_dir/$report_name.md
+mv -v $TEST_LITESTER_PATH/my.md $TEST_LITESTER_PATH/$report_name.md
 
 # pack all logs
 cd "${TEST_LITESTER_PATH}"
