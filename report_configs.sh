2af146292de4f85688e4af11059b3ad5b48b530b
weilinfox <caiweilin@iscas.ac.cn>
2024-12-17 15:57:04 +0800

    Generate some variables from ruyi variables
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- report_configs.sh
+++ report_configs.sh
@@ -8,6 +8,26 @@
 # @Desc    : Static configs of report
 #####################################
 
+# ruyi variables
+if [ -n "$RUYI_VERSION" ]
+	case "$(uname -m)" in
+		x86_64)
+			RUYI_ARCH="amd64"
+			;;
+		aarch64)
+			RUYI_ARCH="arm64"
+			;;
+		riscv64)
+			RUYI_ARCH="riscv64"
+			;;
+	esac
+	RUYI_LINK="https://mirror.iscas.ac.cn/ruyisdk/ruyi/releases/${RUYI_VERSION}/ruyi.${RUYI_ARCH}"
+
+	TEST_ARCH="$RUYI_ARCH"
+	TEST_PKG_LINK="$RUYI_LINK"
+	TEST_VERSION="$RUYI_VERSION"
+fi
+
 TEST_ARCH="${TEST_ARCH:?}"
 TEST_LITESTER_PATH="${TEST_LITESTER_PATH:?}"
 TEST_PKG_LINK="${TEST_PKG_LINK:?}"
